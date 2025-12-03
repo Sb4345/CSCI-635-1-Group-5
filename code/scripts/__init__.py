aff344a3aee887e211672263dca3d22e1fe698a5
spacechain42 <spacekiddo42@gmail.com>
2025-12-03 09:41:47 -0500

    Model final evaluations notebook
    Created notebook with framework for evaluating saved/trained models.
    
    Helper script created to save and load sklearn models as pickle files.
    MLP model saving and loading from class methods.

--- code/scripts/__init__.py
+++ code/scripts/__init__.py
@@ -3,3 +3,5 @@ from .normalize import normalize_data
 from .sample import sample_stratify, top_pca, resample_data
 from .mlp_model import MLPModel
 from .evaluate import evaluate
+from .load_processed import load_processed_data
+from .save_load_model import save_model, load_model
